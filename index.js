f01a2e88a0870f5e7113761ffdf0932663879a90
Shivani Jatkar <103409001+shivanijatkar@users.noreply.github.com>
2023-10-12 10:25:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function generateOTP() {
+          
+    // Declare a digits variable 
+    // which stores all digits
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 4; i++ ) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    var span = document.querySelector(".container p span");
+    span.innerHTML = OTP;
+}
